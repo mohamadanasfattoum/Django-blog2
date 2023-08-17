014e1046103d26ad5481d2e6cf09d047b3371944
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-17 21:22:02 +0200

    admin.site.register(post) and- add-design post models p1

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+'''
+    - html widght
+    - validation
+    - best for db
 
+'''
 
 class post(models.Model):
     title = models.CharField(max_length=120)
