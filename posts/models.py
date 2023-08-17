f8da557be1bb843f8c41ecaf767b2f77bb86df45
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-17 22:06:39 +0200

    post-models-return self.title

--- posts/models.py
+++ posts/models.py
@@ -13,3 +13,7 @@ class post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(max_length=5000)
     publish_date = models.DateTimeField()
+
+
+    def __str__(self) -> str :
+        return self.title
