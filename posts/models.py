e9265b8d76eb81ca15a4cecc05df89251c3effda
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-18 16:40:54 +0200

    add-User

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
 
 '''
     - html widght
@@ -10,6 +10,7 @@ from django.db import models
 '''
 
 class post(models.Model):
+    auther = models.ForeignKey(User,related_name='post_auther',on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=120)
     content = models.TextField(max_length=5000)
     publish_date = models.DateTimeField()
