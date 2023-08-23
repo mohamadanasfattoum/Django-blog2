835293dc2d2212e727c4185b7aaad2a715cdce17
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-23 23:14:26 +0200

    add def add_post in views

--- posts/views.py
+++ posts/views.py
@@ -13,3 +13,9 @@ def post_list(request):
 def post_detail(request,post_id):
     data = post.objects.get(id=post_id)
     return render(request,'post_detail.html',{'post':data})
+
+
+
+
+def add_post(request):
+    form = PostForm()
