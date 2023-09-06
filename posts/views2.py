941aacb5339497dbfa23e34ffc2855287d8e91b4
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-06 20:44:09 +0200

    add analysis template & context in views2.py

--- posts/views2.py
+++ posts/views2.py
@@ -1,6 +1,10 @@
 from django.views import generic
 from .models import post
 
+'''
+template: post_list.html
+context: post_list , object_list
+'''
+
 class PostList(generic.ListViews):
     modol = post   # Query: database
-    
