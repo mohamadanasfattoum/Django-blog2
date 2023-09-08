eb4dd7f98a7482634449b3ea72324d6abe0223aa
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-08 16:35:58 +0200

    add PostDelete in views2

--- posts/views2.py
+++ posts/views2.py
@@ -33,3 +33,8 @@ class PostUpdate(generic.UpdateView):
     fields='__all__'
     success_url= '/blog/'
     template_name='posts/edit.html'
+
+
+
+class PostDelete(generic.DeleteView):
+    model= post
