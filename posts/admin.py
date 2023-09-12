92702c4c9b1b728091bece0b4e65db25643b03ed
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-12 16:33:40 +0200

    add list_display in class PostAdmin in admin

--- posts/admin.py
+++ posts/admin.py
@@ -4,6 +4,7 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'publish_date']
 
 admin.site.register(Post)
 admin.site.register(Comment)
