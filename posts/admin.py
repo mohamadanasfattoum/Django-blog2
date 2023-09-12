d6b1fe62056953faf87bae6b8ab090fe142e3965
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-12 16:36:56 +0200

    add summernote_fields = '__all__' in class PostAdmin in admin

--- posts/admin.py
+++ posts/admin.py
@@ -5,6 +5,7 @@ from .models import Post, Comment
 
 class PostAdmin(SummernoteModelAdmin):
     list_display = ['title' , 'publish_date']
+    summernote_fields = '__all__'
 
 admin.site.register(Post,PostAdmin)
 admin.site.register(Comment)
