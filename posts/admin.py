bdf0e85d2c779991e6e7ee235da8db2277d492e9
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-12 16:36:16 +0200

    add (SummernoteModelAdmin in class PostAdmin in admin

--- posts/admin.py
+++ posts/admin.py
@@ -3,8 +3,8 @@ from django_summernote.admin import SummernoteModelAdmin
 
 from .models import Post, Comment
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ['title' , 'publish_date']
 
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
 admin.site.register(Comment)
