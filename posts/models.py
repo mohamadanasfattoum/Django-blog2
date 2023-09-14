b7e06ff99b3fc90ca8c71a3ab925f3c49a7a752e
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-14 21:14:24 +0200

    add TextField(max_length=50000)

--- posts/models.py
+++ posts/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     auther = models.ForeignKey(User,related_name='post_auther',on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=120)
     subtitle = models.CharField(max_length=1000, null=True)
-    content = models.TextField(max_length=5000)
+    content = models.TextField(max_length=50000)
     publish_date = models.DateTimeField(default=timezone.now)
     tags = TaggableManager()
     image = models.ImageField(upload_to='posts',null=True)
